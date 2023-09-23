b63c92068e4d277a65e6f78d499edd77d46ac648
beholdenkey <justthered63@gmail.com>
2023-09-23 13:28:37 -0400

    feat: updated script

--- hack/cluster-destroy.sh
+++ hack/cluster-destroy.sh
@@ -4,15 +4,12 @@ DIR="$(dirname "$0")"
 source "${DIR}/logging.sh"
 source "${DIR}/helper.sh"
 
-# Default cluster name
-readonly DEFAULT_CLUSTER_NAME="media-cluster"
-
-# Override cluster name with first script argument, if provided
-CLUSTER_NAME=${1:-${DEFAULT_CLUSTER_NAME}}
+# Use environment variable for cluster name
+CLUSTER_NAME=${CLUSTER_NAME}
 
 # Validate the cluster name
-if [[ ! ${CLUSTER_NAME} =~ ^[a-zA-Z0-9-]+$ ]]; then
-    log_err "Invalid cluster name: ${CLUSTER_NAME}. It must contain only alphanumeric characters and hyphens."
+if [[ -z ${CLUSTER_NAME} || ! ${CLUSTER_NAME} =~ ^[a-zA-Z0-9-]+$ ]]; then
+    log_err "Invalid or empty cluster name: ${CLUSTER_NAME}. It must contain only alphanumeric characters and hyphens."
     exit 1
 fi
 
